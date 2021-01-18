19a279c3b6e6d931aaeaf61454a6554541024224
Tomati <dopedup@mail.ru>
2021-01-18 16:16:29 +0100

    Project structure initial commit

---
+++ cosmetolog/src/StaticPageElements/Footer.js
@@ -0,0 +1,8 @@
+import './Styles/Footer.css';
+
+export default function Footer(props) {
+
+  return(
+    <h1>Thi is footer</h1>
+  );
+}
---
+++ cosmetolog/src/StaticPageElements/Header.js
@@ -0,0 +1,8 @@
+import './Styles/Header.css';
+
+export default function Header(props) {
+
+  return(
+    <h1>Thi is header</h1>
+  );
+}
