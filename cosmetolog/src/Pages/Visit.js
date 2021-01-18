b1cc669ad9dfaf59932bf1c4dd8b20030e3389a0
Tomati <dopedup@mail.ru>
2021-01-18 17:16:01 +0100

    Visit Page v1

--- cosmetolog/src/Pages/Visit.js
+++ cosmetolog/src/Pages/Visit.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './Styles/Visit.css';
 
 export default class Visit extends React.Component {
 
@@ -9,7 +10,27 @@ export default class Visit extends React.Component {
 
   render() {
     return(
-      <h1>This is Visit!</h1>
+      <div className="visit">
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="firstNameLabel">Имя</span>
+          <input id="firstName" type="text" className="form-control" placeholder="Женя" aria-label="FirstName" aria-describedby="firstNameLabel"/>
+        </div>
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="lastNameLabel">Фамилия</span>
+          <input id="lastName" type="text" className="form-control" placeholder="Лалка" aria-label="LastName" aria-describedby="lastNameLabel"/>
+        </div>
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="mailLabel">E-mail</span>
+          <input id="mail" type="text" className="form-control" placeholder="exemple@mail.ru" aria-label="Email" aria-describedby="mailLabel" />
+        </div>
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="phoneLabel">Телефон</span>
+          <input id="phone" type="text" className="form-control" placeholder="+375290071994" aria-label="Phone" aria-describedby="phoneLabel" />
+        </div>
+        <div className="send-button-wrapper">
+          <button type="button" className="btn btn-primary">Записаться</button>
+        </div>
+      </div>
     );
   }
 }
